499da80114e1e5483321cab759e403120a50b101
AlbertoMavungo <mavungo1998@gmail.com>
2022-03-24 08:39:41 -0300

    primeiro commit

---
+++ EX1_2_3.java
@@ -0,0 +1,17 @@
+public class EX2_1_3 {
+    public static void main(Integer [] args) {
+        // declaração
+        int contador;
+        //inicilição
+        contador = 0;
+
+        //operaçoes
+        while (contador <= 100) {
+          contador ++;
+          System.out.printf("%d\n",contador);
+        }   
+         
+        
+    }
+     
+}
---
+++ EX3_3.java
@@ -0,0 +1,12 @@
+public class EX3_3 {
+    public static void main(String []args) {
+     //declarando  
+     int numero, resultado;     
+     //entrada de dados
+     numero = Integer.parseInt("entre com algum número inteiro");
+     //operações
+      for (numero = numero; numero >=1; numero--);
+        resultado = numero * numero-1;
+        
+    }
+}
